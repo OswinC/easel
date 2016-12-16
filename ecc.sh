642fc953547d7059634d7980601372895a1a3da7
Danielle Crosswell <dcrosswell95@gmail.com>
2016-12-16 18:32:14 -0500

    run ecc.sh with -c or no options

--- ecc.sh
+++ ecc.sh
@@ -4,24 +4,36 @@ EASEL="./easel.native"
 filename=`echo $2 | sed 's/.*\\///
                            s/.es$//'`
 
-while getopts "al" c; do
+compile() {
+    # Compile easel to .ll file
+    $EASEL -l < $filename.es > $filename.ll
+
+    #Compile to assembly code
+    llc -o $filename.s $filename.ll 
+
+    #Compile the assembly code and link it with lib glwrap
+    gcc -o $filename $filename.s -Lglwrap/_build -lglwrap -lGL -lGLEW -lglut -lm 
+
+    #Clean up the generated file
+    rm $filename.ll $filename.s
+}
+
+if (! getopts "alc" c); then
+    filename=`echo $1 | sed 's/.*\\///
+                           s/.es$//'`
+    compile
+fi
+
+while getopts "alc" c; do
     case $c in
-	l) # Compile easel to .ll file
+	l) 
+        # Compile easel to .ll file
 	$EASEL -l < $filename.es > $filename.ll
-
-	#Compile to assembly code
-	llc -o $filename.s $filename.ll 
-
-	#Compile the assembly code and link it with lib glwrap
-	gcc -o $filename $filename.s -Lglwrap/_build -lglwrap -lGL -lGLEW -lglut -lm 
-
-	#Clean up the generated file
-	rm $filename.ll $filename.s
 	    ;;
 	a) # Test the pretty-printing of AST
         $EASEL -a < $filename.es > $filename.ast
 	    ;;
+        c) compile 
+	    ;;
     esac
 done 
-
-
