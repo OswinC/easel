c516009aab39d80b1fd9f544b6f148248ce568a2
Oswin Chou <oswinchou@gmail.com>
2016-12-17 00:47:59 -0500

    Add -o option and usage message for ecc.sh
    Usage: ecc.sh [options] <.es files>
        -c               Compile to executable (default)
        -l               Output LLVM IR code
        -a               Output pretty-printing of AST
        -o <file name>   Specify output file name
        -h               Print this help

--- ecc.sh
+++ ecc.sh
@@ -1,39 +1,66 @@
 #!/bin/sh
 
 EASEL="./easel.native"
-filename=`echo $2 | sed 's/.*\\///
-                           s/.es$//'`
+
+usage() {
+    echo "Usage: ecc.sh [options] <.es files>"
+    echo "-c               Compile to executable (default)"
+    echo "-l               Output LLVM IR code"
+    echo "-a               Output pretty-printing of AST"
+    echo "-o <file name>   Specify output file name"
+    echo "-h               Print this help"
+    exit 1
+}
 
 compile() {
     # Compile easel to .ll file
-    $EASEL -l < $filename.es > $filename.ll
+    $EASEL < "$fullpath" > "$basename".ll && 
 
     #Compile to assembly code
-    llc -o $filename.s $filename.ll 
+    llc -o "$basename".s "$basename".ll &&
 
     #Compile the assembly code and link it with lib glwrap
-    gcc -o $filename $filename.s -Lglwrap/_build -lglwrap -lGL -lGLEW -lglut -lm 
+    gcc -o "$output" "$basename".s -Lglwrap/_build -lglwrap -lGL -lGLEW -lglut -lm
 
     #Clean up the generated file
-    rm $filename.ll $filename.s
+    rm "$basename".ll "$basename".s > /dev/null 2>&1
 }
 
-if (! getopts "alc" c); then
-    filename=`echo $1 | sed 's/.*\\///
-                           s/.es$//'`
-    compile
-fi
-
-while getopts "alc" c; do
+# ==================== Start of variable setup
+act="" 
+while getopts "alco:" c; do
     case $c in
-	l) 
-        # Compile easel to .ll file
-	$EASEL -l < $filename.es > $filename.ll
-	    ;;
-	a) # Test the pretty-printing of AST
-        $EASEL -a < $filename.es > $filename.ast
-	    ;;
-        c) compile 
-	    ;;
+	l) [ -n "$act" ] && usage || act="l";;
+	a) [ -n "$act" ] && usage || act="a";;
+	c) [ -n "$act" ] && usage || act="c";;
+    o) output="$OPTARG" ;;
+    *) usage ;;
     esac
 done 
+[ -z "$act" ] && act="c"
+
+shift `expr $OPTIND - 1`
+# Parameters appearing after double dash are positional parameters
+[ "$1" = "--" ] && shift
+
+[ $# -ne 1 ] && usage
+
+fullpath="$1"
+basename=`echo "$fullpath" | sed 's/.*\\///
+                                    s/.es$//'`
+# ==================== End of variable setup
+
+case "$act" in
+    l) 
+        [ -z "$output" ] && output="$basename".ll
+        $EASEL -l < "$fullpath" > "$output"
+        ;;
+    a)
+        [ -z "$output" ] && output="$basename".ast
+        $EASEL -a < "$fullpath" > "$output"
+        ;;
+    c)
+        [ -z "$output" ] && output="$basename"
+        compile
+        ;;
+esac
